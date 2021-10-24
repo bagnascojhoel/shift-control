e27afac38d96db1b666e8464443c3b22ac3579a4
Jhoel Bagnasco <jhoel.bagnasco@cwi.com.br>
2021-10-24 19:55:38 -0300

    refact: remove unecessary code

--- src/ui/components/total-time-card/total-time-card.tsx
+++ src/ui/components/total-time-card/total-time-card.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Box, Text } from 'native-base';
-import { StyleSheet } from 'react-native';
-
 import { Time24Hours } from '@model';
-import { $V, $C } from '@global-styles';
 
 export function TotalTimeCard({ totalMinutes = 0 }: { totalMinutes: number }) {
   const DefaultText = (
-    <Text style={TotalTimeCardStyles.text}>Você ainda não fez nada</Text>
+    <Text color="orange.500" fontWeight="bold" fontSize="3xl">
+      Você ainda não tem horas registradas
+    </Text>
   );
 
   return (
@@ -27,32 +26,3 @@ export function TotalTimeCard({ totalMinutes = 0 }: { totalMinutes: number }) {
     </Box>
   );
 }
-
-const Shared = StyleSheet.create({
-  text: {
-    color: $C.white,
-    fontWeight: 'bold',
-  },
-});
-
-const TotalTimeCardStyles = StyleSheet.create({
-  container: {
-    marginVertical: $V.smallGutter,
-    paddingHorizontal: $V.gutter,
-    paddingVertical: $V.smallGutter,
-    borderRadius: 30,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-
-  time: {
-    ...Shared.text,
-    marginHorizontal: $V.smallGutter,
-    fontSize: $V.fontSizeExtra,
-  },
-
-  text: {
-    ...Shared.text,
-    fontSize: $V.fontSizeLarge,
-  },
-});
